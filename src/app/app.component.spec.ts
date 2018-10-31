0b36d236a729fff2a8a13944659563d68ad389f6
Marvin Fromenteau <marvin-dbz@hotmail.fr>
2018-10-31 17:51:49 +0100

    fix build

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'stuDIYFront'`, () => {
+  it(`should have as title 'Material-Angular-PWA'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('stuDIYFront');
+    expect(app.title).toEqual('Material-Angular-PWA');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to stuDIYFront!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Material-Angular-PWA!');
   });
 });
