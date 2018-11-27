810d611bfef534aea968302008f32ca63b3c54fe
Marvin Fromenteau <marvin-dbz@hotmail.fr>
2018-11-27 14:45:36 +0100

    fix

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { CrudGenericService } from '../CrudGeneric/crud-generic.service';
+import { Http } from '@angular/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService extends CrudGenericService {
 
-  constructor() {
-    super('user');
+  constructor(http: Http) {
+    super(http, 'user');
   }
 }
